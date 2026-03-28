87479b22fa970020f10d3db67130e9bed5a97d9d
Prabhat Soni <52910557+prabhatsn1@users.noreply.github.com>
2026-03-28 19:11:19 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,4 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  stream: require.resolve("stream-browserify"),
-};
-
 module.exports = config;
